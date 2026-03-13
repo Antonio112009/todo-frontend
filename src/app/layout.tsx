2a603c532d438dcc6bfeab101c16202f9529813a
antonio112009 <29264726+Antonio112009@users.noreply.github.com>
2026-03-13 19:15:33 +0000

    Fix Inter font: use className instead of variable for direct application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
+        className={`${inter.className} ${jetbrainsMono.variable} antialiased`}
       >
         {children}
       </body>
